1724456fb150b9009639c03f4bac57271d037477
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-01 09:34:21 +0530

    Update greatest_of_3_no.c

--- .c/greater_b/greatest_of_3_no.c
+++ .c/greater_b/greatest_of_3_no.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    // input three numbers//
+    // Takng input first number//
     int a;
     printf("Enter the value of a:");
     scanf("%d", &a);
 
+    // Takng input Second number//
     int b;
     printf("Enter the value of b:");
     scanf("%d", &b);
+
     
+    // Takng input third number//
     int c;
     printf("Enter the value of c:");
-    scanf("%d", &c); // compare of three numbers//
+    scanf("%d", &c); 
+    
+    // compare of three numbers//
     if (a > b && a > c)
     {
         printf("a is greatest");
