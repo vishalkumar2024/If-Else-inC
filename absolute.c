f99a81f2d2b57d8464323a7251f719804b92b5fa
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-02 09:24:37 +0530

    Update absolute.c

--- absolute.c
+++ absolute.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main (){
+    
+    // taking input the number
     int n;
     printf("enter the number :");
     scanf("%d",&n);
+    
+    // converting the no. into absolute no.
     if (n<0){
         n=n*(-1);
     }
