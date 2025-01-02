0a9324d0c1d9d27c70587a572c5e3c208cdc3918
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-02 09:27:38 +0530

    updating the comments

--- side_of_triangle.c
+++ side_of_triangle.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main (){
+    // Taking input all the sides
     int a,b,c;
     printf("enter the values of a,b and c:");
     scanf("%d %d %d",&a,&b,&c);
+
+    // checking the formation of sides can be a triangle or not
     if (a+b>c && b+c>a && a+c>b){
         printf("valid traingle ");
     }
