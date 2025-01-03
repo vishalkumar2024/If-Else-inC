969ac4fc0d7d4ce2d16860be97b47733d7ea1c41
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-03 09:15:26 +0530

    Update ternary.c

--- ternary.c
+++ ternary.c
@@ -1,10 +1,13 @@
 // code of ternary operator //
 #include<stdio.h>
 int main(){
+    
+    // Taking two number input
     int a,b;
     printf("enter the value of a and b ");
     scanf("%d %d",&a,&b);
+    
+    // Ternary operator
     a>b?printf("a is greater"):printf("b is greater ");
-
     return 0;
     }
