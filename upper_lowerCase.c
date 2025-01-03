05f731e52504b56b7b5326f021f22095871d4107
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-03 09:17:56 +0530

    Update comments

--- upper_lowerCase.c
+++ upper_lowerCase.c
@@ -1,8 +1,11 @@
+// This is the code checking input character is upperCase or lowerCase
 #include <stdio.h>
 int main (){
+ // Taking input the character 
     char ch;
     printf("enter the character :");
     scanf("%c",&ch);
+ // Checking....
 if(ch>='a' && ch<='z'){
     printf("lower case ");
 }
